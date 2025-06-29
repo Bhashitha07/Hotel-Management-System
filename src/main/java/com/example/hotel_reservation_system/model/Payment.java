d31d07387f8e36a121eb553cc72eaf4a9821d65b
IT24100229 <bhashithapraharshana2@gmail.com>
2025-06-29 21:32:46 +0530

    Create Payment.java

---
+++ src/main/java/com/example/hotel_reservation_system/model/Payment.java
@@ -0,0 +1,43 @@
+package com.example.hotel_reservation_system.Payment;
+
+import java.util.Date;
+
+public class Payment {
+    private String paymentMethod;
+    private double amount;
+    private Date date;
+    private String status;
+
+    // Getters and Setters
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
